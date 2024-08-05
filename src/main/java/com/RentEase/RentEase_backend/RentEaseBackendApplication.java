2f6ed10f5e2a9ea6a9bd6dae58ce9b35c7c395f1
Amrit Raj <amrit.raj1504@gmail.com>
2024-08-05 21:22:12 +0530

    project-initialization

---
+++ src/main/java/com/RentEase/RentEase_backend/RentEaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.RentEase.RentEase_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentEaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentEaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RentEase/RentEase_backend/RentEaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RentEase.RentEase_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentEaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
