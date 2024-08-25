1c660063484db5970fdd3ee397102dc754c357a1
Amrit Raj <amrit.raj1504@gmail.com>
2024-08-25 14:08:30 +0530

    origin changed

--- src/main/java/com/rentease_server/server/config/WebConfig.java
+++ src/main/java/com/rentease_server/server/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Adjust this path according to your API endpoints
-                .allowedOrigins("http://localhost:5173", "https://rent-ease-client-two.vercel.app")
+                .allowedOrigins("http://localhost:5173", "https://rent-ease-client-two.vercel.app", "https://renteasebackend-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
